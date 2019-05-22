189d9ce3e39d471d1ee96359b64b530045ecc009
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-21 20:28:41 -0700

    built out base file structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import Component from './Component';
+
+
+
+class App extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <!-- Header goes here -->
+
+                <main>
+                    <!-- CatList goes here -->
+                </main>
+            </div>
+        `;
+    }
+}
